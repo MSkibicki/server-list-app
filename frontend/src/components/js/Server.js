b8945283b5c29683acaf10a2ef1126ea74755c28
MSkibicki <m.skibicki777@gmail.com>
2020-08-19 18:31:32 +0200

    install classnames utility

--- frontend/src/components/js/Server.js
+++ frontend/src/components/js/Server.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Server.scss";
 import ServerMenu from "./ServerMenu";
 import axios from "axios";
+import classNames from "classnames";
 
 const Server = ({ name, status, id }) => {
+  const [serverStatus, setServerStatus] = useState(null);
+
   const serverOn = () => {
     axios
       .put(`http://localhost:5000/servers/${id}/on`, { status: "ONLINE" })
       .then((response) => {
-        console.log(response);
+        setServerStatus(response.data.status);
       });
   };
 
@@ -16,17 +19,27 @@ const Server = ({ name, status, id }) => {
     axios
       .put(`http://localhost:5000/servers/${id}/off`, { status: "OFFLINE" })
       .then((response) => {
-        console.log(response);
+        setServerStatus(response.data.status);
       });
   };
+
+  const serverClass = classNames({
+    "online": status === "ONLINE",
+    "offline": status === "OFFLINE",
+    "put-online": serverStatus !== null && serverStatus === "ONLINE",
+    "put-offline": serverStatus !== null && serverStatus === "OFFLINE",
+  });
 
   return (
     <li className="server">
       <h1>{name}</h1>
-      <h3 className={`${status === "ONLINE" ? "online" : "offline"}`}>
-        {status}
-      </h3>
-      <ServerMenu serverOn={serverOn} serverOff={serverOff} status={status} />
+      <h3 className={serverClass}>{serverStatus ? serverStatus : status}</h3>
+      <ServerMenu
+        serverOn={serverOn}
+        serverOff={serverOff}
+        status={status}
+        serverStatus={serverStatus}
+      />
     </li>
   );
 };
